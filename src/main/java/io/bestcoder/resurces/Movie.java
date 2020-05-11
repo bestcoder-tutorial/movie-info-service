2dfcfd977988d4f409d7033a0e92cbe27ae949e9
das2016 <bachouche.skander@gmail.com>
2020-05-11 02:05:56 +0200

    Commit : add movie info service

---
+++ src/main/java/io/bestcoder/resurces/Movie.java
@@ -0,0 +1,12 @@
+package io.bestcoder.resurces;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Movie {
+
+	private String id;
+	private String name;
+}
