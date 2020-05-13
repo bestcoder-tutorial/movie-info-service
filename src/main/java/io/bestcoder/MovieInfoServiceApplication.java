2a15560d28fa15a83b21bb749c8b4d74786f5fbc
das2016 <bachouche.skander@gmail.com>
2020-05-13 02:41:25 +0200

    Commit : eureka client example

--- src/main/java/io/bestcoder/MovieInfoServiceApplication.java
+++ src/main/java/io/bestcoder/MovieInfoServiceApplication.java
@@ -2,8 +2,10 @@ package io.bestcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//optional @EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
